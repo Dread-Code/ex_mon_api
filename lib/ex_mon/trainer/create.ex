8db89b9d78ba6868b0861ef0165240cb50a9228d
DreadfulCrazy <lucasospina97@gmail.com>
2022-05-12 00:33:25 -0500

    Error handler for create trainer

--- lib/ex_mon/trainer/create.ex
+++ lib/ex_mon/trainer/create.ex
@@ -8,5 +8,5 @@ defmodule ExMon.Trainer.Create do
   end
 
   defp create_trainer({:ok, struct}), do: Repo.insert(struct)
-  defp create_trainer({:error, _struct}), do: :error
+  defp create_trainer({:error, _changeset} = error), do: error
 end
--- lib/ex_mon_web/views/error_view.ex
+++ lib/ex_mon_web/views/error_view.ex
@@ -1,5 +1,6 @@
 defmodule ExMonWeb.ErrorView do
   use ExMonWeb, :view
+  import Ecto.Changeset, only: [traverse_errors: 2]
 
   # If you want to customize a particular status code
   # for a certain format, you may uncomment below.
@@ -12,5 +13,17 @@ defmodule ExMonWeb.ErrorView do
   # "Not Found".
   def template_not_found(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
+  end
+
+  def render("400.json", %{result: %Ecto.Changeset{} = result}) do
+    %{message: translate_errors(result)}
+  end
+
+  defp translate_errors(changeset) do
+    traverse_errors(changeset, fn {msg, opts} ->
+      Regex.replace(~r"%{(\w+)}", msg, fn _, key ->
+        opts |> Keyword.get(String.to_existing_atom(key), key) |> to_string()
+      end)
+    end)
   end
 end
