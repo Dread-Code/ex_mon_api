821309b7841cea0abf8cecd82c72a3221b31b681
DreadfulCrazy <lucasospina97@gmail.com>
2022-05-12 21:24:30 -0500

    Delete controller created

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -2,5 +2,6 @@ defmodule ExMon do
   alias ExMon.Trainer
 
   defdelegate create_trainer(params), to:  Trainer.Create, as: :call
+  defdelegate delete_trainer(params), to:  Trainer.Delete, as: :call
 
 end
