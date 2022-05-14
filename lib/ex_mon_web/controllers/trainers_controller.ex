c73d5a8b0e19926089880eeac4d66e6c12b36e89
DreadfulCrazy <lucasospina97@gmail.com>
2022-05-14 10:54:01 -0500

    adding some parameter to handle response for reuse more

--- lib/ex_mon_web/controllers/trainers_controller.ex
+++ lib/ex_mon_web/controllers/trainers_controller.ex
@@ -6,7 +6,7 @@ defmodule ExMonWeb.TrainersController do
    def create(conn, params) do
      params
      |> ExMon.create_trainer
-     |> handle_response(conn)
+     |> handle_response(conn, "create.json", :created)
    end
 
    def delete(conn, %{"id" => id}) do
@@ -23,12 +23,17 @@ defmodule ExMonWeb.TrainersController do
 
    defp handle_delete({:error, _reason} = error, _conn), do: error
 
-
-   defp handle_response({ :ok, trainer }, conn) do
-      conn
-      |> put_status(:created)
-      |> render("create.json", trainer: trainer)
+   def show(conn, %{"id" => id}) do
+      id
+      |> ExMon.fetch_trainer
+      |> handle_response(conn, "show.json", :ok)
    end
 
-   defp handle_response({:error, _changeset} = error, _conn), do: error
+   defp handle_response({ :ok, trainer }, conn, view, status) do
+      conn
+      |> put_status(status)
+      |> render(view, trainer: trainer)
+   end
+
+   defp handle_response({:error, _changeset} = error, _conn, _view, _status), do: error
 end
