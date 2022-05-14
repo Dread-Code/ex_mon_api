9eee93f62286a0c63d7c391b77930d2a133fd6a9
DreadfulCrazy <lucasospina97@gmail.com>
2022-05-14 10:49:07 -0500

    Get for trainers

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -3,5 +3,6 @@ defmodule ExMon do
 
   defdelegate create_trainer(params), to:  Trainer.Create, as: :call
   defdelegate delete_trainer(params), to:  Trainer.Delete, as: :call
+  defdelegate fetch_trainer(params), to:  Trainer.Get, as: :call
 
 end
