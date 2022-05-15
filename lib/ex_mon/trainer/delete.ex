cbe6daffe085bcd8909948192226be1694f98260
DreadfulCrazy <lucasospina97@gmail.com>
2022-05-15 13:45:11 -0500

    Refactor for get trainer by module Get

--- lib/ex_mon/trainer/delete.ex
+++ lib/ex_mon/trainer/delete.ex
@@ -1,20 +1,13 @@
 defmodule ExMon.Trainer.Delete do
-  alias ExMon.{Trainer, Repo}
-  alias Ecto.UUID
+  alias ExMon.{ Repo}
 
   def call(id) do
-    case UUID.cast(id) do
-       :error -> {:error, "Invalid ID Format"}
-       {:ok, uuid} -> delete(uuid)
+    case ExMon.fetch_trainer(id) do
+       {:error, reason} -> {:error, reason}
+       {:ok, trainer} -> delete_trainer(trainer)
     end
   end
 
-  defp delete(uuid) do
-    case fetch_trainer(uuid) do
-      nil -> {:error, "Trainer not found"}
-      trainer -> Repo.delete(trainer)
-    end
-  end
+  defp delete_trainer(trainer), do: Repo.delete(trainer)
 
-  defp fetch_trainer(uuid), do: Repo.get(Trainer, uuid)
 end
