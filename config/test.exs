eaeec705e56a29ffd3e27a29166c855a819f7357
DreadfulCrazy <lucasospina97@gmail.com>
2022-05-24 14:44:04 -0500

    Schema Trainer tests

--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,8 @@ use Mix.Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :ex_mon, ExMon.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "dreadfulcrazy",
+  password: "",
   database: "ex_mon_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
