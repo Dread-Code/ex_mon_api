49190d4d5c68f8013b2e546234e23dbd9d8909e3
DreadfulCrazy <lucasospina97@gmail.com>
2022-05-24 12:10:20 -0500

    Delete route

--- lib/ex_mon_web/controllers/fallback_controller.ex
+++ lib/ex_mon_web/controllers/fallback_controller.ex
@@ -1,10 +1,7 @@
 defmodule ExMonWeb.FallbackController do
   use ExMonWeb, :controller
-  require Logger
 
   def call(conn, {:error, message, status, view}) do
-    Logger.info(message)
-    Logger.info("sasdfasfafsf")
     conn
     |> put_status(status)
     |> put_view(ExMonWeb.ErrorView)
--- lib/ex_mon_web/controllers/trainer_pokemons_controller.ex
+++ lib/ex_mon_web/controllers/trainer_pokemons_controller.ex
@@ -1,6 +1,5 @@
 defmodule ExMonWeb.TrainerPokemonsController do
   use ExMonWeb, :controller
-  require Logger
 
 
   action_fallback ExMonWeb.FallbackController
@@ -16,6 +15,20 @@ defmodule ExMonWeb.TrainerPokemonsController do
     |> ExMon.fetch_trainer_pokemon
     |> handle_response(conn, "show.json", :ok)
   end
+
+  def delete(conn, %{"id" => id}) do
+    id
+    |> ExMon.delete_trainer_pokemon
+    |> handle_delete(conn)
+  end
+
+  defp handle_delete({:ok, _trainer}, conn) do
+    conn
+    |> put_status(:no_content)
+    |> text("")
+ end
+
+defp handle_delete({:error, reason}, _conn), do: {:error, reason, 400, 400}
 
   defp handle_response({ :ok, pokemon }, conn, view, status) do
     conn
