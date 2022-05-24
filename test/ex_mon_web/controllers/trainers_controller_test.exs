b9408d1b29761594df85ceae0bc6a80a0819e57d
DreadfulCrazy <lucasospina97@gmail.com>
2022-05-24 15:47:55 -0500

    Get controller tested

---
+++ test/ex_mon_web/controllers/trainers_controller_test.exs
@@ -0,0 +1,29 @@
+defmodule ExMonWeb.Controllers.TrainersControllerTest do
+  use ExMonWeb.ConnCase
+  alias ExMon.Trainer
+
+  describe "show/2" do
+    test "when there is a trainer the given id return a trainer", %{conn: conn} do
+      params = %{name: "Rafael", password: "1234456"}
+      {:ok, %Trainer{id: id}} = ExMon.create_trainer(params)
+
+      response =
+        conn
+        |> get(Routes.trainers_path(conn, :show, id))
+        |> json_response(:ok)
+
+      assert %{"id" => id, "inserted_at" => _inserted_at, "name" => "Rafael"} = response
+    end
+
+    test "when there is an error, returns error", %{conn: conn} do
+      response =
+        conn
+        |> get(Routes.trainers_path(conn, :show, "1234"))
+        |> json_response(:bad_request)
+
+      expected_response = %{"errors" => %{"detail" => "Bad Request"}}
+
+      assert expected_response = response
+    end
+  end
+end
