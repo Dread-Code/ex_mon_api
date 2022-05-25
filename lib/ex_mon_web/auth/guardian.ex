c76c7908f560618123cfd8a25cfed3c5683af943
DreadfulCrazy <lucasospina97@gmail.com>
2022-05-24 19:47:31 -0500

    Sign in route created

--- lib/ex_mon_web/auth/guardian.ex
+++ lib/ex_mon_web/auth/guardian.ex
@@ -1,6 +1,8 @@
 defmodule ExMonWeb.Auth.Guardian do
   use Guardian, otp_app: :ex_mon
 
+  alias ExMon.Trainer
+
   def subject_for_token(trainer, _claims) do
     # You can use any value for the subject of your token but
     # it should be useful in retrieving the resource later, see
@@ -16,4 +18,24 @@ defmodule ExMonWeb.Auth.Guardian do
     |> Map.get("sub")
     |> ExMon.fetch_trainer
   end
+
+  def authenticate(%{"id" => trainer_id, "password" => password}) do
+    case ExMon.fetch_trainer(trainer_id) do
+      {:ok, trainer} -> validate_password(trainer, password)
+      {:error, reason} -> {:error, reason, 400, 400}
+    end
+  end
+
+  defp validate_password(%Trainer{password_hash: hash} = trainer, password) do
+    case Argon2.verify_pass(password, hash) do
+      true -> create_token(trainer)
+      false -> {:error, "unauthorized", 401, 400}
+    end
+  end
+
+  defp create_token(trainer) do
+    {:ok, token, _clains} = encode_and_sign(trainer)
+    {:ok, token}
+  end
+
 end
--- lib/ex_mon_web/router.ex
+++ lib/ex_mon_web/router.ex
@@ -10,6 +10,7 @@ defmodule ExMonWeb.Router do
     pipe_through(:api)
     resources("/trainers", TrainersController, only: [:create, :show, :delete, :update])
     resources("/trainer_pokemons", TrainerPokemonsController, only: [:create, :show, :delete, :update])
+    post("/trainers/signin", TrainersController, :sign_in)
     get("/pokemons/:name", PokemonsController, :show)
   end
 
